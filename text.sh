6e7c87e7bc1ae5d1a0ff8bdf7443e5ad00eb5ca1
pauldchang <153890789+pauldchang@users.noreply.github.com>
2024-05-21 20:52:26 -0500

    text.sh

--- text.sh
+++ text.sh
@@ -1,1 +1,14 @@
-example-here
+#!/bin/bash
+set -e
+
+if [ "$1" = 'postgres' ]; then
+    chown -R postgres "$PGDATA"
+
+    if [ -z "$(ls -A "$PGDATA")" ]; then
+        gosu postgres initdb
+    fi
+
+    exec gosu postgres "$@"
+fi
+
+exec "$@"
